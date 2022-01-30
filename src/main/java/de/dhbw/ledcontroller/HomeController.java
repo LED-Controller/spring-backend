e566b08e5e2b277b902e61388d872e65ca0c849e
Simon <simon2022@github.com>
2022-01-30 23:20:41 +0100

    fix formatting

--- src/main/java/de/dhbw/ledcontroller/HomeController.java
+++ src/main/java/de/dhbw/ledcontroller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@RequestMapping("/")
-    public String home(){
-        return "LED-Controller";
-    }
-	
+	public String home() {
+		return "LED-Controller";
+	}
+
 }
