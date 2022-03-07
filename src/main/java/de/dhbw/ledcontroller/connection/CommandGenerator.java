1dc316f9bb2fac2012fd1ff74c5d460e9ae9f4f6
Simon <59486823+simon2022@users.noreply.github.com>
2022-03-07 13:03:18 +0100

    fix test

--- src/main/java/de/dhbw/ledcontroller/connection/CommandGenerator.java
+++ src/main/java/de/dhbw/ledcontroller/connection/CommandGenerator.java
@@ -1,10 +1,5 @@
 package de.dhbw.ledcontroller.connection;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
-import org.junit.jupiter.api.Test;
-
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -43,14 +38,6 @@ public class CommandGenerator {
 		String toReplace = "{" + placeholder + "}";
 		input = input.replace(toReplace, replace);
 		return input;
-	}
-
-	@Test
-	void test() {
-		assertThat(CommandGenerator.colorRGB(1, 2, 3), is("F1 1 2 3"));
-		assertThat(CommandGenerator.colorRGBW(1, 2, 3, 4), is("F2 1 2 3 4"));
-		assertThat(CommandGenerator.colorNeoRGB(1, 2, 3), is("X1 1 2 3"));
-		assertThat(CommandGenerator.off(), is("OFF"));
 	}
 
 }
